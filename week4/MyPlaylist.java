83eb5c013d4d1e5109fa8032561ae06d4a547d75
Dhool143 <155487698+Dhool143@users.noreply.github.com>
2024-05-01 18:08:17 -0700

    Add files via upload

---
+++ week4/MyPlaylist.java
@@ -0,0 +1,19 @@
+package week4;
+
+public class MyPlaylist {
+    public static void main(String[] args) {
+        
+
+int playlist = 1;
+
+while(playlist <= 10){
+     System.out.println("playing my songs!");
+     playlist ++;
+
+
+}
+
+System.out.println("Finished playing my songs!");
+
+    }
+}
